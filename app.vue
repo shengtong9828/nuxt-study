0ed102d764fb87816190721dad2f62d6bcc1e02c
shengtong <shengtong9828@163.com>
2023-06-03 21:08:49 +0800

    fix:环境变量测试

--- app.vue
+++ app.vue
@@ -2,5 +2,14 @@
   <div>
     <!-- <NuxtWelcome /> -->
     <h1>hello nuxt</h1>
+    <button @click="a">test</button>
   </div>
 </template>
+
+<script setup>
+function a() {
+  console.log(process, "process");
+  console.log(process.env, "process.env");
+  console.log(process.env.NOTION_CONFIG, "process.env.NOTION_CONFIG");
+}
+</script>
